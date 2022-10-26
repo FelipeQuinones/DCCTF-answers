421bc136e00baf764058d1e7c24391531555705f
FelipeQuinones <fquiones@uc.cl>
2022-10-26 11:18:01 -0300

    changed name variables

--- clock.py
+++ clock.py
@@ -1,9 +1,10 @@
 from hashlib import md5
+from time import time
 
 for h in range(0,25):
     for m in range(0,61):
         for s in range(0, 61):
-            resst = f"{h}:{m}:{s}"
-            res = md5(resst.encode())
-            if res.hexdigest() == "5bc3223650036e1efe6733b585746bf8":
-                print(resst)
+            time_ = f"{h}:{m}:{s}"
+            time_to_hash = md5(time_.encode())
+            if time_to_hash.hexdigest() == "5bc3223650036e1efe6733b585746bf8":
+                print(time_)
